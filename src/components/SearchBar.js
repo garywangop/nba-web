411a5f57bd34b99217e124e4128ddf28ace37804
garywangop <garywangop@gmail.com>
2020-02-15 13:04:05 -0500

    Auto-complete step 2: option list.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,36 @@
 import React, {Component} from 'react';
-import { Icon, Input, AutoComplete } from "antd"
+import { Icon, Input, AutoComplete } from "antd";
+import nba from '../nba-client';
+import { PROFILE_PIC_URL_PREFIX } from '../constants';
+
+const { Option } = AutoComplete;
 
 class SearchBar extends Component {
+    state = {
+        dataSource: [],
+    }
+
+    handleSearch = value => {
+        const players = nba.searchPlayers(value);
+        this.setState({
+            dataSource: !value ?
+                [] : nba.searchPlayers(value).map(player => ({
+                    fullName: player.fullName,
+                    playerId: player.playerId,
+                }))
+        });
+
+    }
+
     render() {
+        const {dataSource} = this.state;
+        const options = dataSource.map(player => (
+            <Option key={player.fullName} value={player.fullName} className="player-option">
+                <img className="player-option-image" src={`${PROFILE_PIC_URL_PREFIX}/${player.playerId}.png`}/>
+                <span className="player-option-label">{player.fullName}</span>
+            </Option>
+        ));
+
         return (
             <div>
                 <AutoComplete
@@ -10,6 +38,8 @@ class SearchBar extends Component {
                     size="large"
                     placeholder="Search NBA Player"
                     optionLabelProp="value"
+                    onSearch={this.handleSearch}
+                    dataSource={options}
                 >
                     <Input suffix={ <Icon type="search" className="certain-category-icon"/>}/>
                 </AutoComplete>
