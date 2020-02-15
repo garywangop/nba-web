e0b1f0ef8ec756133b38ef2e7379c4faeb116d29
garywangop <garywangop@gmail.com>
2020-02-15 13:23:22 -0500

    Auto-complete step 3: select option

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
-import { Icon, Input, AutoComplete } from "antd";
+import { AutoComplete, Input, Icon } from 'antd';
 import nba from '../nba-client';
 import { PROFILE_PIC_URL_PREFIX } from '../constants';
 
-const { Option } = AutoComplete;
+const Option = AutoComplete.Option;
 
 class SearchBar extends Component {
     state = {
         dataSource: [],
     }
 
-    handleSearch = value => {
+    handleSearch = (value) => {
         const players = nba.searchPlayers(value);
+        console.log(players);
+
         this.setState({
             dataSource: !value ?
                 [] : nba.searchPlayers(value).map(player => ({
@@ -19,31 +21,33 @@ class SearchBar extends Component {
                     playerId: player.playerId,
                 }))
         });
+    }
 
+    onSelect = (name) => {
+        this.props.handleSelectPlayer(name);
     }
 
     render() {
-        const {dataSource} = this.state;
-        const options = dataSource.map(player => (
+        const { dataSource } = this.state;
+        const options = dataSource.map((player) => (
             <Option key={player.fullName} value={player.fullName} className="player-option">
                 <img className="player-option-image" src={`${PROFILE_PIC_URL_PREFIX}/${player.playerId}.png`}/>
                 <span className="player-option-label">{player.fullName}</span>
             </Option>
         ));
-
         return (
-            <div>
-                <AutoComplete
-                    className="search-bar"
-                    size="large"
-                    placeholder="Search NBA Player"
-                    optionLabelProp="value"
-                    onSearch={this.handleSearch}
-                    dataSource={options}
-                >
-                    <Input suffix={ <Icon type="search" className="certain-category-icon"/>}/>
-                </AutoComplete>
-            </div>
+            <AutoComplete
+                className="search-bar"
+                dataSource={options}
+                onSelect={this.onSelect}
+                onSearch={this.handleSearch}
+                placeholder="Search NBA Player"
+                size="large"
+                optionLabelProp="value"
+            >
+
+                <Input suffix={<Icon type="search" className="certain-category-icon" />} />
+            </AutoComplete>
         );
     }
 }
