0ac0d71030edf020b4b0483a42068d017909574b
garywangop <garywangop@gmail.com>
2020-02-15 12:31:36 -0500

    Auto-complete step 1: input bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import { Icon, Input, AutoComplete } from "antd"
 
 class SearchBar extends Component {
     render() {
         return (
             <div>
-                
+                <AutoComplete
+                    className="search-bar"
+                    size="large"
+                    placeholder="Search NBA Player"
+                    optionLabelProp="value"
+                >
+                    <Input suffix={ <Icon type="search" className="certain-category-icon"/>}/>
+                </AutoComplete>
             </div>
         );
     }
